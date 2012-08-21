7ef4ccc75a4bfc62fd9a9763fb2f8062f55af41e
Garrow Bedrossian <garrowb@freshybox.(none)>
2012-08-21 11:22:15 +1000

    Add script to install case-insensitive completion in /etc/inputrc

---
+++ installers/ignore-case-completion.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function install_nocase_completion()
+{
+	local inputrc="/etc/inputrc"
+	local nocasecommand="set completion-ignore-case on"
+	local install_date=`date +'%Y%m%d'` 
+	local install_string="\n# Automatically installed by $0 -- $install_date\n$nocasecommand\n"
+
+	if [[ -f $inputrc ]] ; then
+		grep --silent  "$nocasecommand" $inputrc
+		gres=$?
+		if [ $gres -gt 0 ] ; then
+			echo "Not installed, installing"
+
+			echo -e $install_string  > $inputrc
+		fi
+		echo $inputrc
+	fi
+}
+
+install_nocase_completion
+
