a321992c1ae792b8c839f61393da251c1ee1b67d
Garrow Bedrossian <garrow.bedrossian@hitwise.com>
2012-08-21 12:23:12 +1000

    disable fasd

--- ubuntu/ubuntu.bashrc.sh
+++ ubuntu/ubuntu.bashrc.sh
@@ -106,12 +106,12 @@ complete -o bashdefault -o default -o nospace -F _git g
 
 
 #### FASD
-fasd_cache="$HOME/.fasd-init-bash"
-if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
-  fasd --init posix-alias bash-hook bash-ccomp bash-ccomp-install >| "$fasd_cache"
-fi
-source "$fasd_cache"
-unset fasd_cache
+#fasd_cache="$HOME/.fasd-init-bash"
+#if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
+#  fasd --init posix-alias bash-hook bash-ccomp bash-ccomp-install >| "$fasd_cache"
+#fi
+#source "$fasd_cache"
+#unset fasd_cache
 
 
 ## Rake completion attempts - disabled for the moment for speed.
