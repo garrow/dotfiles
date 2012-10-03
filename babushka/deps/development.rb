fa226f04650a38881f9cf498e90c70a2108fe748
Garrow Bedrossian <garrow.bedrossian@hitwise.com>
2012-10-03 12:34:08 +1000

    Starting point for development dependencies

--- babushka/deps/development.rb
+++ babushka/deps/development.rb
@@ -1,11 +1,28 @@
 dep 'development' do
-	
+  requires 'python'
 end
 
 
 dep 'postgresql.bin' do
   provides 'psql'
 end
+
+dep 'python' do
+  requires 'python.bin', 'python-psycopg2.lib', 'pylint.bin'
+end
+
+dep 'python.bin'
+dep 'pylint.bin'
+dep 'python-psycopg2.lib'
+
+
+dep 'php' do
+
+  requires 'php5-cli.lib'
+end
+
+
+  dep 'php5-cli.lib'
 
 # postgresql-contrib
 # pgadmin3
