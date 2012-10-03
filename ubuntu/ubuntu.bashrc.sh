7861da9c95c5ff5a4f8cab4f33f99babba4a0a26
Garrow Bedrossian <garrow.bedrossian@hitwise.com>
2012-10-03 12:35:36 +1000

    Disable lesspipe, I think for git output to not always fill the screen...

--- ubuntu/ubuntu.bashrc.sh
+++ ubuntu/ubuntu.bashrc.sh
@@ -21,7 +21,7 @@ HISTFILESIZE=2000
 shopt -s checkwinsize
 
 # make less more friendly for non-text input files, see lesspipe(1)
-[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+#[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
 
 # set variable identifying the chroot you work in (used in the prompt below)
 if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
