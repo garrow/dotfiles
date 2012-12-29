9208c32c9c35a72056e8a09672f0870be9a65a39
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2012-12-30 10:00:59 +1100

    Remove commented out PS1 commands

--- core/prompt.sh
+++ core/prompt.sh
@@ -41,19 +41,6 @@ GIT_PS1_SHOWDIRTYSTATE=1     # *
 GIT_PS1_SHOWSTASHSTATE=1     # $
 GIT_PS1_SHOWUNTRACKEDFILES=1 # %
 
-#REPO_STATUS='(${prompt_char})'
-# \[\033[01;34m\]
-
-#PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME}: ${PWD}\007"'
-
-#PROMPT_COMMAND='echo -ne "\w \$(rb_ver)\$(rails_e)"'
-#PS1="\[\033[01;34m\]\w\[\033[00m\] \$(__git_ps1 \"<%s>\" )$Red\$(rb_ver)$Blue\$(rails_e)$Color_Off\$(prompt_char) "
 PS1="$IBlue\w$Color_Off \$(__git_ps1 \"<%s>\" )$Red\$(rb_ver)$Blue\$(rails_e)\$(stomp_e)$Color_Off\$(prompt_char) "
-#PS1="\[\033[01;34m\]\w\[\033[00m\] $Red\$(rb_ver)$Blue\$(rails_e)$Color_Off\$(prompt_char) "
 PROMPT_COMMAND='echo -ne "\033]0;$(prompt_char) ${PWD} $(rb_ver)$(rails_e)\007"'
 
-
-
-#PS1='\[\033[01;34m\]\w\[\033[00m\] \[\033[01;32m\]$(rb_ver)$(rails_e)$(prompt_char) '
-#PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] $(rails_e)$(prompt_char) '
-#PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]${REPO_STATUS} '
