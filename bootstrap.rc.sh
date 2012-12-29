334ad2a41761cfa6284dfc28b874fb4c0aa16f8e
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2012-12-29 16:08:28 +1100

    First pass at reorganising for dual system dotfiles

--- bootstrap.rc.sh
+++ bootstrap.rc.sh
@@ -2,11 +2,21 @@ source /etc/profile
 
 export base_env_path=${HOME}/bin/env
 
-# Hard coded for the moment
-export bashrc_location=${base_env_path}/ubuntu/ubuntu.bashrc.sh
+if [ $(uname) == 'Darwin' ]
+then
+	echo "OSX"
+	export distro=osx
+else
+	export distro=ubuntu
+	echo "Assume ubuntu"
+fi
+ 
+export bashrc_location=${base_env_path}/${distro}/bashrc.sh
 
-# export DEBUG_DOTFILES=true
+ export DEBUG_DOTFILES=true
 
 if [ -f $bashrc_location ]; then
   source $bashrc_location
 fi
+
+PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
