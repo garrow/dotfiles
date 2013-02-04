d93fbfd57919b3e1ea14bb2b3d5a1375c2c23810
Garrow Bedrossian <garrow.bedrossian@hitwise.com>
2013-02-04 11:00:11 +1100

    Add 'cob' checkout branch menu command

---
+++ plugins/git.sh
@@ -0,0 +1,12 @@
+alias __git_clean_branch_list="git branch --list --color=never |tr -d '*'"
+alias cob=__git_checkout_branch_menu
+
+function __git_checkout_branch_menu(){
+PS3="Select a branch or Ctrl+C to cancel: "
+	select opt in $branches;
+	do
+		git checkout ${opt};
+		break;
+	done
+}
+
