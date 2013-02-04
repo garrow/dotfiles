9a562f5c17cd14f2081f36a22006d66a5bac39c2
Garrow Bedrossian <garrow.bedrossian@hitwise.com>
2013-02-04 11:07:53 +1100

    Silly syntax error

--- plugins/git.sh
+++ plugins/git.sh
@@ -1,8 +1,9 @@
 alias __git_clean_branch_list="git branch --list --color=never |tr -d '*'"
 alias cob=__git_checkout_branch_menu
 
-function __git_checkout_branch_menu(){
+function __git_checkout_branch_menu()
+{
 PS3="Select a branch or Ctrl+C to cancel: "
-select opt in $(__git_clean_branch_list; do git checkout ${opt}; break;	done
+select opt in $(__git_clean_branch_list); do git checkout ${opt}; break;	done
 }
 
