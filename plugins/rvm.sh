c2686cdff94498f66aa4a4e25153ed351bdec3f0
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2013-03-12 21:02:04 +1100

    Why did I comment out the RVM path setting?

--- plugins/rvm.sh
+++ plugins/rvm.sh
@@ -1,6 +1,6 @@
 # RVM
 source "${HOME}/.rvm/scripts/rvm"
-#PATH=$HOME/.rvm/bin:$PATH
+PATH=$HOME/.rvm/bin:$PATH
 alias load_rvm='source "${HOME}/.rvm/scripts/rvm"'
 alias rd='rvm . do'
 alias rad='rvm all do'
