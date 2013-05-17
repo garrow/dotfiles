0b5c6fae6f1276f43ad1150bb7d57396bb5aa5de
Garrow Bedrossian <garrow.bedrossian@hitwise.com>
2013-05-17 10:02:16 +1000

    Add chruby plugin to replace rvm

---
+++ plugins/chruby.sh
@@ -0,0 +1,16 @@
+# chruby plugin, loads chruby and provides prompt helper function
+source /usr/local/share/chruby/chruby.sh
+
+function rb_ver {
+	if [ "$RUBY_VERSION" == "" ]; then
+		echo ""
+	elif [ "$RUBY_ENGINE" == "jruby" ]; then
+		echo "♨"
+	elif [ "$RUBY_VERSION" == "1.9.3" ]; then
+		echo "⚑"
+	else
+		echo "ᴚ"
+	fi
+    #echo "◇"
+}
+
