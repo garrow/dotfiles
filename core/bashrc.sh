a4e18ebcb7f8abd1f8515597398d43373d57ddc7
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2013-08-14 14:01:38 +1000

    Ensure /usr/local/bin precedes default paths.

--- core/bashrc.sh
+++ core/bashrc.sh
@@ -54,7 +54,7 @@ done
 
 source ~/.extras.rc.sh
 
-PATH="~/bin/:$PATH"
+PATH="~/bin/:/usr/local/bin:$PATH"
 # enable programmable completion features (you don't need to enable
 # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
 # sources /etc/bash.bashrc).
