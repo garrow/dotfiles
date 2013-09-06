10bcd904b6ab01d80a71ab9f7e2cecee41772927
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2013-09-06 16:35:54 +1000

    Clean up Ruby version flag char.

--- plugins/chruby.sh
+++ plugins/chruby.sh
@@ -4,13 +4,15 @@ source /usr/local/share/chruby/auto.sh
 
 function rb_ver {
 	if [ "$RUBY_VERSION" == "" ]; then
-		echo ""
+		echo "ᴚ"
 	elif [ "$RUBY_ENGINE" == "jruby" ]; then
 		echo "♨"
+	elif [ "$RUBY_VERSION" == "2.0.0" ]; then
+		echo ""
 	elif [ "$RUBY_VERSION" == "1.9.3" ]; then
-		echo "⚑"
+		echo "ᴿ"
 	else
-		echo "ᴚ"
+		echo "ℛ "
 	fi
     #echo "◇"
 }
