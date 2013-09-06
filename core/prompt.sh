7905554ff22f0f02b125fe7626a537c43813b8cc
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2013-09-06 16:41:45 +1000

    Cleaner prompt, only show CWD

--- core/prompt.sh
+++ core/prompt.sh
@@ -7,11 +7,10 @@ function short_path {
 }
 
 function prompt_char {
-    if [[ -d './.svn/' ]]; then echo '§' && return; fi
     git branch >/dev/null 2>/dev/null && echo '±' && return
     #hg root >/dev/null 2>/dev/null && echo '☿' && return
-#    svn info  >/dev/null 2>/dev/null && echo '§' && return
-    echo '○' 
+    #if [[ -d './.svn/' ]]; then echo '§' && return; fi
+    echo '○'
 }
 
 
@@ -22,23 +21,16 @@ function rb_ver {
 
 function rails_e {
   if [ "$RAILS_ENV" != '' ]; then
-  echo "‡$RAILS_ENV " && return 
+    echo "‡$RAILS_ENV " && return
   fi
 }
 
-
-function stomp_e {
-  if [ "$STOMPR_WEB_APP_CONFIG" != '' ]; then
-	echo "$(basename $STOMPR_WEB_APP_CONFIG)"
-  fi
-}
 
 #### GIT CONFIG
-GIT_PS1_SHOWDIRTYSTATE=1     # * 
+GIT_PS1_SHOWDIRTYSTATE=1     # *
 GIT_PS1_SHOWSTASHSTATE=1     # $
 GIT_PS1_SHOWUNTRACKEDFILES=1 # %
 
-PS1="$IPurple\$(short_path)$Color_Off \$(__git_ps1 \"<%s>\" )$Red\$(rb_ver)$Blue\$(rails_e)\$(stomp_e)$Color_Off\$(prompt_char) "
-#PS1="$IBlue\w$Color_Off \$(__git_ps1 \"<%s>\" )$Red\$(rb_ver)$Blue\$(rails_e)\$(stomp_e)$Color_Off\$(prompt_char) "
-PROMPT_COMMAND='echo -ne "\033]0;$(prompt_char) ${PWD} $(rb_ver)$(rails_e)\007"'
+PS1="$IPurple\W$Color_Off\$(__git_ps1 \"⌥ %s\" )$Red\$(rb_ver)$Blue\$(rails_e)$Color_Off$Green\$(prompt_char)$Color_Off "
+#PROMPT_COMMAND='echo -ne "\033]0;$(prompt_char) ${PWD} $(rb_ver)$(rails_e)\007"'
 
