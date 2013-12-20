09f284d3546753f52ba9b3a1c788c4e0a5b0aaf1
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2013-12-20 15:25:14 +1100

    Add 'cor' Checkout Remote Branch Menu. cc @fredwu @thil @jamesmoriarty

--- plugins/git.sh
+++ plugins/git.sh
@@ -1,4 +1,3 @@
-
 alias g=git
 alias gs='git status'
 alias gse='git status --ignored'
@@ -12,17 +11,25 @@ alias grp='git reset --patch'
 alias gcp='git checkout --patch'
 alias gvp=gcp
 
-alias __git_clean_branch_list="git branch  --color=never |tr -d '*'"
-alias cob=__git_checkout_branch_menu
-alias com='git checkout master'
 alias gmod="git diff --name-status |grep -v '^D' | tr -s '\t' |cut -f2"
+# Branch management
+alias cob=__git_checkout_branch_menu
+alias cor=__git_checkout_remote_branch_menu
+alias com='git checkout master'
 
-
-# TODO - Ensure this can handle detached HEAD states.
+alias __git_branch_list="git branch --list --color=never"
+alias __git_local_branch_list="__git_branch_list |tr -d ' *' | grep -v '(no branch)'"
+alias __git_remote_branch_list="__git_branch_list --remotes | grep -v 'origin/master' | cut -d/ -f 2-"
+alias __git_only_remote_branch_list="cat  <(__git_local_branch_list) <(__git_local_branch_list) <(__git_remote_branch_list) |sort |uniq -u"
 
 __git_checkout_branch_menu()
 {
 PS3="Select a branch or Ctrl+C to cancel: "
-select opt in $(__git_clean_branch_list); do git checkout ${opt}; break;	done
+select opt in $(__git_local_branch_list); do git checkout ${opt}; break; done
 }
 
+__git_checkout_remote_branch_menu()
+{
+PS3="Select a branch or Ctrl+C to cancel: "
+select opt in $(__git_only_remote_branch_list); do git checkout ${opt} ; break; done
+}
