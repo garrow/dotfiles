957caaaae411f211158946f6e09ff0e849bb3ae6
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2014-02-05 11:28:47 +1100

    Add dotfiles helper alias.

--- core/aliases.sh
+++ core/aliases.sh
@@ -26,6 +26,7 @@ alias tod='date +%H:%M:%S'
 ### Misc tools
 alias v=vim
 alias rld="source ~/.bashrc"
+alias dotfiles="cd ~/.dotfiles/"
 alias cwd="pwd | tr -d '\n' | pbcopy"
 alias dud="du -hcd 1"
 alias pyserve="python -m SimpleHTTPServer 8000"
