faaf38e2a3a35b26798bb3e0ac161ac3deed9131
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2014-02-10 17:17:12 +1100

    Add gbc - git branch changes, list all files changed on branch.

--- plugins/git.sh
+++ plugins/git.sh
@@ -11,7 +11,13 @@ alias grp='git reset --patch'
 alias gcp='git checkout --patch'
 alias gvp=gcp
 
+# Changed files
+alias __git_current_branch='git rev-parse --abbrev-ref HEAD'
+alias __git_current_branch_revisions='git rev-list $(__git_current_branch) ^master'
+
+alias gbc="git diff --name-only $(__git_current_branch_revisions) master"
 alias gmod="git diff --name-status |grep -v '^D' | tr -s '\t' |cut -f2"
+
 # Branch management
 alias cob=__git_checkout_branch_menu
 alias cow=__git_checkout_working_branches_menu
