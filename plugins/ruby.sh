b4f6f30c092bfeb40075207cc8b01e61f2ef3a33
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2014-02-20 11:09:14 +1100

    Set bundler parallel jobs to 8.

--- plugins/ruby.sh
+++ plugins/ruby.sh
@@ -2,6 +2,8 @@ export RUBY_GC_MALLOC_LIMIT=90000000
 export RUBY_FREE_MIN=200000
 export RUBY_GC_HEAP_FREE_SLOTS=200000
 
+export BUNDLE_JOBS=8
+
 alias spec="time rspec"
 alias s=spec
 alias be="bundle exec"
