3996c055f163322fd7b4300b88c4bfb1fbd37449
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2014-02-20 11:10:19 +1100

    Erase history dupes rather than ignoring.

--- core/bashrc.sh
+++ core/bashrc.sh
@@ -3,7 +3,7 @@
 
 # don't put duplicate lines in the history. See bash(1) for more options
 # ... or force ignoredups and ignorespace
-HISTCONTROL=ignoredups:ignorespace
+HISTCONTROL=erasedups:ignorespace
 
 # append to the history file, don't overwrite it
 shopt -s histappend
