1bcc4717656c72cdd3ee74facc6ccce2d0184103
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2014-05-15 12:12:12 +1000

    Move Babushka deps to own repo at last.

--- babushka/deps/dep_helper.rb
+++
@@ -1,8 +0,0 @@
-def dependency_package name, deps
-  deps.each { |d|
-    dep d
-  }
-  dep name.to_s do
-    requires *deps
-  end
-end
--- babushka/deps/meta.rb
+++
@@ -1,7 +0,0 @@
-dep 'env' do
-  requires 'desktop-environment'
-end
-
-dep 'desktop-environment' do
-	requires 'terminal', 'desktop', 'development'
-end
