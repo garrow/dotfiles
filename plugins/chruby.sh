4d8a9631b1ef02204d8ffe18cc20c138ecd4da13
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2014-05-21 15:12:03 +1000

    Update my default ruby to 2.1.1

--- plugins/chruby.sh
+++ plugins/chruby.sh
@@ -1,18 +1,19 @@
 # chruby plugin, loads chruby and provides prompt helper function
 source /usr/local/share/chruby/chruby.sh
 source /usr/local/share/chruby/auto.sh
+export DEFAULT_RUBY_VERSION="2.1.1"
 
 function rb_ver {
 	if [ "$RUBY_VERSION" == "" ]; then
 		echo "ᴚ"
 	elif [ "$RUBY_ENGINE" == "jruby" ]; then
 		echo "♨ "
-	elif [ "$RUBY_VERSION" == "2.0.0" ]; then
+	elif [ "$RUBY_VERSION" == $DEFAULT_RUBY_VERSION ]; then
 		echo ""
 	elif [ "$RUBY_VERSION" == "1.9.3" ]; then
 		echo "ᴿ"
 	else
-		echo "ℛ "
+		echo "ℛ  $RUBY_VERSION"
 	fi
     #echo "◇"
 }
