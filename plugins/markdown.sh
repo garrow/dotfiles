89e526e1c47249db057fcd624ddd9f653843ca27
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2014-07-04 17:44:22 +1000

    Add mardown inliner functions

---
+++ plugins/markdown.sh
@@ -0,0 +1,19 @@
+
+github_markdown_template_file="${base_env_path}/plugins/tp-markdown.html"
+
+function github_markdown()
+{
+   pandoc --from markdown_github --to html --template $github_markdown_template_file $@
+}
+
+function premailer()
+{
+  python -m premailer --disable-validation  --remove-star-selectors
+}
+
+function target_process_html()
+{
+  github_markdown $@ | premailer | pbcopy
+}
+
+
