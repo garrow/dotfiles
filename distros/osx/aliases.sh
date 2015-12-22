070bbddd8f810417d897a912c43b3cde3c4d9fc3
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-12-23 10:50:02 +1100

    default ls alias sort by time

--- distros/osx/aliases.sh
+++ distros/osx/aliases.sh
@@ -1,5 +1,5 @@
 alias ls_custom="ls -lh"
-alias l="ls_custom -o"  # Suppress group ID
+alias l="ls_custom -ot"  # Suppress group ID
 alias ll="l -a"         # Almost everything.
 alias la="ls_custom -a" # Everything
 
