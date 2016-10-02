6814c0c6fda777b4e63ee750c4db3e572b4e88af
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2016-10-03 10:11:52 +1100

    Add quick play/pause to spotify control

--- plugins/spotify.sh
+++ plugins/spotify.sh
@@ -1,10 +1,17 @@
 # The very awesome https://github.com/dronir/SpotifyControl
 alias spotify="osascript ${base_env_path}/plugins/SpotifyControl.scpt"
 alias m=__spotify_menu
+alias mp="spotify play/pause"
 
 __spotify_menu()
 {
   SPOTIFY_COMMANDS=('play/pause' 'next' 'previous' 'info' 'shuffle' 'repeat');
   PS3="$(spotify --) >> "
-  select opt in "${SPOTIFY_COMMANDS[@]}"; do spotify ${opt}; break; done
+
+  if [[ "$1" == "p" ]]; then
+    spotify "play/pause"
+  else
+    select opt in "${SPOTIFY_COMMANDS[@]}"; do spotify ${opt}; break; done
+  fi
+
 }
