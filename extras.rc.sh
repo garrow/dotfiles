c50a64b39c83a4cdd7e5762c1d3c621fd09c2324
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2017-07-21 10:53:51 -0400

    Replace old projects with new projects

--- extras.rc.sh
+++ extras.rc.sh
@@ -1,16 +1,7 @@
-alias extras="vim ~/.extras.rc.sh"
+alias extras="mvim ~/.extras.rc.sh"
 
-
-alias mos="cd ~/work/code/mid_office_system"
-alias cbt="cd ~/work/code/cbt"
-alias sp="cd ~/work/code/smartpoint"
-alias tmp="cd ~/work/code/travel_management_platform"
-alias mof="cd ~/work/mid-office/mos-requirements"
-alias mdb='psql mapping_lighting_development'
-alias piit='cd ~/Dropbox/learn/uni2013/2015/ICT80008'
-alias insight='cd /Users/garrow/work/code/insight'
-alias maven=mvn
 alias bgop="cd /Users/garrow/Dropbox/work/code/bgop"
+alias pico8="cd /Users/garrow/Dropbox/Projects/code/pico8"
 alias ksr="cd /Users/garrow/work/code/kickstarter"
 alias wiki="cd /Users/garrow/work/code/wiki "
 
@@ -19,19 +10,16 @@ alias emulate_production='cp config/settings/production.yml config/settings/deve
 
 alias tdl='tail -f log/development.log'
 alias ttl='tail -f log/test.log'
+alias tpl='tail -f ~/Library/Logs/Pow/*.log'
 
 alias deps="cd ~/.babushka/deps"
 alias todo="vim ~/todo.txt"
 alias mmod='gmod | spelunk'
 
+
+
 # Obsoleted by brew services
 alias stop_pg='pg_ctl stop -D /usr/local/var/postgres'
-
-
-alias fame="cd ~/fame/"
-alias fw="cd ~/fame/website"
-alias fdb="psql fame_website_development"
-alias reset_fame_database="pg_restore -d fame_website_development --clean ~/fame/data/database_backups/development/fame_website_development_clean.pgdump"
 
 alias findd='find . -type d |grep -i'
 
@@ -53,12 +41,65 @@ bundle exec rake test TEST="${1}"
 
 function quick_pr(){
   local pr_file=$(mktemp -t NEW_PR)
-  local current_branch=$(git rev-parse --abbrev-ref HEAD)
+  local __git_current_branch=$(git rev-parse --abbrev-ref HEAD)
 
   git log --pretty="%s" -n 1 > ${pr_file}
-  echo "Creating new PR for branch [ ${current_branch} ]"
+  echo "Creating new PR for branch [ ${__git_current_branch} ]"
+  git push --set-upstream origin ${__git_current_branch}
   cat $pr_file
 
   hub pull-request -F ${pr_file}
+}
 
+
+
+alias cleardevlog="cat /dev/null > log/development.log"
+rt() {
+  bin/spring rake test TEST=$1 TESTOPTS="--name=\"/$2/\""
+}
+
+alias twl="tail -f log/webpack.log"
+function __browserpack
+{
+  # Yarn's website color
+  __set_iterm_tab_color 33 136 182
+  __set_iterm_jobname "webpack📦"
+  ksr
+  cd browser && yarn start
+  ksr
+  __reset_iterm_tab
+  __reset_iterm_jobname
+}
+
+function __yarn_test
+{
+  #__set_iterm_tab_color 33 136 182
+  __set_iterm_tab_color 128 244 66
+  __set_iterm_jobname "yarn✅❌"
+  ksr
+  cd browser && yarn test
+  ksr
+  __reset_iterm_tab
+  __reset_iterm_jobname
+}
+
+function __sidekiq
+{
+  __set_iterm_tab_color 177 0 62
+  __set_iterm_jobname "sidekiq"
+  ksr
+  bin/sidekiq
+  __reset_iterm_tab
+  __reset_iterm_jobname
+}
+
+alias kdb=__kickstarter_dev_database
+
+function __kickstarter_dev_database
+{
+  __set_iterm_tab_color 242 145 17
+  __set_iterm_jobname 'mysql'
+  mysql ksr_dev
+  __reset_iterm_tab
+  __reset_iterm_jobname
 }
