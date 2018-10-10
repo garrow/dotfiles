c4a8781c6db89ecc7adc0371d97d4e82403cb789
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2018-10-10 11:26:23 -0400

    Add direnv_init to setup direnv for .env files

--- plugins/direnv.sh
+++ plugins/direnv.sh
@@ -11,3 +11,16 @@ _direnv_hook() {
 if ! [[ "$PROMPT_COMMAND" =~ _direnv_hook ]]; then
   PROMPT_COMMAND="_direnv_hook;$PROMPT_COMMAND";
 fi
+
+function direnv_init() {
+  local envrcfile="$PWD/.envrc"
+
+  if [ -f $envrcfile  ]; then
+    echo ">>>Existing file<<<<"
+    echo
+    cat $envrcfile
+  elif [ -f "$PWD/.env" ]; then
+    echo "Installing dotenv plugin for envrc"
+    echo "dotenv" >> $envrcfile
+  fi
+}
