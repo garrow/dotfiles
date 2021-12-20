b2a954dc6986aefcdb7cfe4c4adf39c70b5b9a55
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2021-12-20 11:09:19 -0500

    Changed to editor fn

--- shared/plugins/editor.sh
+++ shared/plugins/editor.sh
@@ -1,33 +1,37 @@
+# shellcheck shell=bash 
 function editor {
-  echo $EDITOR
+  # echo "$EDITOR"
 
-  if [ "$EDITOR" == "" ]; then
-    echo "No EDITOR Environment variable set, checking .env files"
-    echo $PWD
-    calling_directory=$PWD
-    env_file="${PWD}/.env"
+  # # if [ "$EDITOR" == "" ]; then
+  # #   echo "No EDITOR Environment variable set, checking .env files"
+  # #   echo $PWD
+  # #   calling_directory=$PWD
+  # #   env_file="${PWD}/.env"
 
-    echo "$env_file"
-    if [ -e "$env_file" ] ; then
-      echo "EXISTS"
-      eval $(cat "${env_file}" | grep 'EDITOR')
+  # #   echo "$env_file"
+  # #   if [ -e "$env_file" ] ; then
+  # #     echo "EXISTS"
+  # #     eval $(cat "${env_file}" | grep 'EDITOR')
 
-    echo "EDITOR== ${EDITOR}"
-    echo $EDITOR
-    fi 
+  # #   echo "EDITOR== ${EDITOR}"
+  # #   echo "$EDITOR"
+  # #   fi 
+  # # fi
+
+  # echo "FIN"
+
+  if [ "$EDITOR" = "" ]; then
+    >&2 echo "No editor defined!"
+  elif ! command -v "${EDITOR}" > /dev/null; then
+    >&2 echo "Editor is invalid! ${EDITOR}"
+  else
+    echo "${EDITOR}"
   fi
-
-echo "FIN"
-
-if [ "$EDITOR" == "" ]; then
-  echo "Unable to find an editor to open the file"
-  exit 1
-else
-
-  echo "Run editor: ${EDITOR}"
- # $("${EDITOR}" $@)
-fi
-
-
 }
 
+
+function to_editor {
+  only_files | xargs $EDITOR
+}
+
+alias storms='only_files | xargs storm'
