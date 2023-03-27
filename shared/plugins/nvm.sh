1defc9b1cda4b0f8ea08e87f5df004201072dfea
Garrow Bedrossian <garrowb@spotify.com>
2023-03-27 11:54:31 -0400

    Shim NVM loading rather than loading on every shell

--- shared/plugins/nvm.sh
+++ shared/plugins/nvm.sh
@@ -1,11 +1,15 @@
-export NVM_DIR="${HOME}/.nvm"
+# Expect this shell function is overridden when loading by `/usr/local/opt/nvm/nvm.sh`
+nvm() {
+    echo "🟢 🌋 NVM First run"
+    export NVM_DIR="${HOME}/.nvm"
 
-if [ ! -d "${NVM_DIR}" ] ; then
-    mkdir "${NVM_DIR}"
-fi
+    if [ ! -d "${NVM_DIR}" ] ; then
+        echo "Creating NVM_DIR: ${NVM_DIR}"
+        mkdir "${NVM_DIR}"
+    fi
 
-load_dependency_file "/usr/local/opt/nvm/nvm.sh" "🌋 NVM"
-load_dependency_file "/usr/local/opt/nvm/etc/bash_completion.d/nvm" "🌋 NVM Completion"
-
-# [ -s "/usr/local/opt/nvm/nvm.sh" ] && source "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-# [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && source "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+    load_dependency_file "/usr/local/opt/nvm/nvm.sh" "🌋 NVM"
+    load_dependency_file "/usr/local/opt/nvm/etc/bash_completion.d/nvm" "🌋 NVM Completion"
+    echo
+    echo "Loaded NVM! You can now call \`nvm\`"
+}
