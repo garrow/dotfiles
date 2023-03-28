c79ce1b6b98c58f1e2b25d6520281718966c21f9
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2023-03-28 19:13:39 -0400

    Replace makefile with first_install bash script

--- Makefile
+++ Makefile
@@ -1,34 +1,39 @@
-install: install_zsh install_command_line_tools install_applications
+install: 
+	./first_install.sh
 
-install_zsh: 	install_zsh_files	install_core
+menu:
+	./first_install.sh menu
+# install: install_zsh install_command_line_tools install_applications
 
-install_bash: 	install_bash_files	install_core
+# install_zsh: 	install_zsh_files	install_core
 
-install_core: install_git install_vim
+# install_bash: 	install_bash_files	install_core
 
-install_vim:
-	ln -vsnf ${PWD}/vim/vimrc ${HOME}/.vimrc
-	ln -vsnf ${PWD}/vim/gvimrc ${HOME}/.gvimrc
-	ln -vsnf ${PWD}/vim/ ${HOME}/.vim
-	mkdir -p ${HOME}/tmp/vim
+# install_core: install_git install_vim
 
-install_git: 
-	ln -vsnf ${PWD}/git/gitconfig ${HOME}/.gitconfig
-	ln -vsnf ${PWD}/git/gitignore_global ${HOME}/.gitignore_global
+# install_vim:
+# 	ln -vsnf ${PWD}/vim/vimrc ${HOME}/.vimrc
+# 	ln -vsnf ${PWD}/vim/gvimrc ${HOME}/.gvimrc
+# 	ln -vsnf ${PWD}/vim/ ${HOME}/.vim
+# 	mkdir -p ${HOME}/tmp/vim
 
-install_command_line_tools:
-	cd ${PWD}/homebrew/command-line && brew bundle
+# install_git: 
+# 	ln -vsnf ${PWD}/git/gitconfig ${HOME}/.gitconfig
+# 	ln -vsnf ${PWD}/git/gitignore_global ${HOME}/.gitignore_global
 
-install_applications:
-	cd ${PWD}/homebrew/gui-apps && brew bundle
+# install_command_line_tools:
+# 	cd ${PWD}/homebrew/command-line && brew bundle
+
+# install_applications:
+# 	cd ${PWD}/homebrew/gui-apps && brew bundle
  
-install_zsh_files:
-	ln -vsnf ${PWD}/zsh/.zshenv ${HOME}/.zshenv
+# install_zsh_files:
+# 	ln -vsnf ${PWD}/zsh/.zshenv ${HOME}/.zshenv
 
-install_bash_files:
-	ln -vsnf ${PWD}/bash/bootstrap.rc.sh ${HOME}/.bash_profile
-	ln -vsnf ${PWD}/extras.rc.sh ${HOME}/.extras.rc.sh
-	ln -vsnf ${PWD}/input.rc ${HOME}/.inputrc
+# install_bash_files:
+# 	ln -vsnf ${PWD}/bash/bootstrap.rc.sh ${HOME}/.bash_profile
+# 	ln -vsnf ${PWD}/extras.rc.sh ${HOME}/.extras.rc.sh
+# 	ln -vsnf ${PWD}/input.rc ${HOME}/.inputrc
 
 # dependencies:
 # 	sudo apt-get install git vim xclip ack-grep realpath curl colordiff
@@ -43,8 +48,8 @@ test:
 	find . -name '*.sh' | grep -v vim | grep -v vendor | grep -v extras | xargs shellcheck -x
 
 
-bootstrap:
-	./first_install.sh
+# bootstrap:
+# 	./first_install.sh
 
 check:
 	shellcheck ./*.sh
--- first_install.sh
+++ first_install.sh
@@ -1,28 +1,120 @@
+# shellcheck shell=bash
 source "./bootstrap.sh"
+source "./shared/core/task_functions.sh"
 
-echo
-echo "➡️  test for remotes"
-git remote -v
+WORKING_DIR=${PWD}
 
-echo
-echo "Lock in git username"
-git config --local --replace-all user.name "$(git config --file "${PWD}/git/gitconfig" --get user.name)"
-git config --local --replace-all user.email "$(git config --file "${PWD}/git/gitconfig" --get user.email)"
-git config --local --get user.name
-git config --local --get user.email
+function install() {
+  install_zsh_config
+  install_vim_config
+  install_git_config
+
+  ensure_directories
+
+  install_homebrew
+  install_cli
+  install_apps
+}
+
+function ensure_directories() {
+  print_info 'ensure_directories'
+  mkdir -p "${HOME}/projects"
+  touch "${HOME}/.extras.rc.sh"
+}
+
+function install_zsh_config() {
+  print_info "ZSH"
+	ln -vsnf "${WORKING_DIR}"/zsh/.zshenv "${HOME}"/.zshenv
+}
+
+function install_vim_config()
+{
+  print_info "VIM Conf"
+	ln -vsnf "${WORKING_DIR}"/vim/vimrc "${HOME}"/.vimrc
+	ln -vsnf "${WORKING_DIR}"/vim/gvimrc "${HOME}"/.gvimrc
+	ln -vsnf "${WORKING_DIR}"/vim/ "${HOME}"/.vim
+	mkdir -p "${HOME}"/tmp/vim
+}
+
+function install_git_config(){ 
+  print_info "git config"
+	ln -vsnf "${WORKING_DIR}"/git/gitconfig "${HOME}"/.gitconfig
+	ln -vsnf "${WORKING_DIR}"/git/gitignore_global "${HOME}"/.gitignore_global
+}
+
+function install_homebrew() {
+  print_info "🍺 Homebrew"
+  if ! command -v brew >/dev/null; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  fi
+}
+
+function install_cli() { 
+  print_info "🖥️  CLI tools"
+  cd "${WORKING_DIR}"/homebrew/command-line && brew bundle
+  cd "${WORKING_DIR}" || return
+}
 
 
-echo
-echo "➡️  install homebrew"
-echo "sudo password required"
-sudo curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | /bin/bash
+function install_apps()
+{
+  print_info "🖥️  Apps"
+  cd "${WORKING_DIR}"/homebrew/gui-apps && brew bundle
+  cd "${WORKING_DIR}" || return
+}
 
-echo
-echo "➡️  which brew"
-which brew
+# ------------- Only available by menu choice -------
+
+#  Unused, but available via menu
+function install_bash() {
+  print_info "BASH"
+  ln -vsnf "${WORKING_DIR}"/bash/bootstrap.rc.sh "${HOME}"/.bash_profile
+	ln -vsnf "${WORKING_DIR}"/extras.rc.sh "${HOME}"/.extras.rc.sh
+	ln -vsnf "${WORKING_DIR}"/input.rc "${HOME}"/.inputrc
+}
+
+function check_repo_config()
+{
+  print_warning "Checking Repo"
+  print_info "➡️  test for remotes"
+  git remote -v
 
 
-echo
-echo "➡️  expected places"
-mkdir -p "${HOME}/projects"
-touch "${HOME}/.extras.rc.sh"
+  print_info "Lock in git username"
+  git config --local --replace-all user.name "$(git config --file "${WORKING_DIR}/git/gitconfig" --get user.name)"
+  git config --local --replace-all user.email "$(git config --file "${WORKING_DIR}/git/gitconfig" --get user.email)"
+  git config --local --get user.name
+  git config --local --get user.email
+}
+
+# ---------- Menu -------
+
+function menu() {
+  echo "Select a command:"
+  for ((i=1; i<${#sub_commands[@]}; i++)); do
+    echo "$i. ${sub_commands[$i]}"
+  done
+  echo "${#sub_commands[@]}. Quit"
+}
+
+# First echo is a no-op to allow 1 index
+sub_commands=(echo install_zsh install_bash install_apps install_cli check_repo_config)
+
+function entry() {
+  if [[ "$1" == "menu" ]]; then
+    menu
+    read choice
+
+    if (( choice == ${#sub_commands[@]} )); then
+      return
+    elif (( choice >= 0 && choice < ${#sub_commands[@]} )); then
+      ${sub_commands[$choice]}
+    else
+      echo "Invalid option. Please try again."
+    fi
+  else
+    install
+  fi
+}
+
+entry "$1"
