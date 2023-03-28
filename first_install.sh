9221c65e57710d58bce89743088769a6fa80baeb
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2023-03-28 18:12:24 -0400

    Add new starship tool for current git author

--- first_install.sh
+++ first_install.sh
@@ -5,6 +5,14 @@ echo "➡️  test for remotes"
 git remote -v
 
 echo
+echo "Lock in git username"
+git config --local --replace-all user.name "$(git config --file "${PWD}/git/gitconfig" --get user.name)"
+git config --local --replace-all user.email "$(git config --file "${PWD}/git/gitconfig" --get user.email)"
+git config --local --get user.name
+git config --local --get user.email
+
+
+echo
 echo "➡️  install homebrew"
 echo "sudo password required"
 sudo curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | /bin/bash
