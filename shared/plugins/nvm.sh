1fcc34e362446b0b6853518a1d206394c9cf027c
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2023-10-06 18:54:02 -0400

    support manual and homebrew installations of nvm

--- shared/plugins/nvm.sh
+++ shared/plugins/nvm.sh
@@ -1,23 +1,22 @@
 # Expect this shell function is overridden when loading by `/usr/local/opt/nvm/nvm.sh`
-nvm() {
-  __nvm_first_run
-}
-
 __nvm_first_run() {
-    echo "🟢 🌋 NVM First run"
+
     export NVM_DIR="${HOME}/.nvm"
 
-    if [ ! -d "${NVM_DIR}" ]; then
-      echo "Creating NVM_DIR: ${NVM_DIR}"
-      mkdir "${NVM_DIR}"
+    if [ -d "${HOMEBREW_PREFIX}/opt/nvm" ]; then
+      echo "🟢 🌋 NVM init (homebrew)"
+      load_dependency_file "${HOMEBREW_PREFIX}/opt/nvm/nvm.sh" "🌋 NVM (homebrew)"
+      load_dependency_file "${HOMEBREW_PREFIX}/opt/nvm/etc/bash_completion.d/nvm" "🌋 NVM Completion (homebrew)"
+    else
+      echo "🟢 🌋 NVM init (manual install)"
+      load_dependency_file "$NVM_DIR/nvm.sh" "🌋 NVM (manual install)"
+      load_dependency_file "$NVM_DIR/bash_completion" "🌋 NVM Completion (manual install)"
+      # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+      # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
     fi
 
-    load_dependency_file "${HOMEBREW_PREFIX}/opt/nvm/nvm.sh" "🌋 NVM"
-    load_dependency_file "${HOMEBREW_PREFIX}/opt/nvm/etc/bash_completion.d/nvm" "🌋 NVM Completion"
-    echo
-    echo "Loaded NVM! You can now call \`nvm\`"
+    type nvm
+    # echo "Loaded NVM! You can now call \`nvm\`"
 }
 
 __nvm_first_run
-
-
