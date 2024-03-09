50d0e90bef41548dd6d633ff999fad700e6ba278
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2024-03-09 14:03:47 -0500

    Add linuxbrew support

--- shared/plugins/homebrew.sh
+++ shared/plugins/homebrew.sh
@@ -4,3 +4,7 @@
 if [[ -f "/opt/homebrew/bin/brew" ]]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
+
+if [[ -f "/home/linuxbrew/.linuxbrew/bin/brew" ]]; then
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
