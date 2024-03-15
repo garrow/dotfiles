4bb86bf1767defe444c299582cb17cd2e1da7c6b
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2024-03-15 16:26:09 -0400

    cleaner apt install

--- install/ubuntu_install.sh
+++ install/ubuntu_install.sh
@@ -1,18 +1,20 @@
 function ubuntu_install() {
   print_info "Ubuntu install"
 
-  print_info "apt-get update"
-  sudo apt-get -qq update
+  print_info "apt update"
+  sudo apt -qq update
 
 
   local required_packages=(git ripgrep direnv build-essential procps curl file xclip tree)
 
   print_info "Installing Packages ${required_packages}"
 
-  for ((i=1; i<=${#required_packages[@]}; i++)); do
-    print_info "  : Install ${required_packages[$i]}"
-    sudo apt-get -qq install -y "${required_packages[$i]}"
-  done
+  # for ((i=1; i<=${#required_packages[@]}; i++)); do
+  #   print_info "  : Install ${required_packages[$i]}"
+  #   sudo apt-get -qq install -y "${required_packages[$i]}"
+  # done
+
+  sudo apt satisfy -y "${required_packages[@]}"
 
   print_info "Starship"
   curl -sS https://starship.rs/install.sh | sh
