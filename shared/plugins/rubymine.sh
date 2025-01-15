c4ccdff107637ab77a59915cc5f589989a304f50
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2025-01-15 11:42:44 -0500

    Make webstorm aliases

--- shared/plugins/rubymine.sh
+++ shared/plugins/rubymine.sh
@@ -1,5 +1,3 @@
 alias spelunk='only_files | xargs mine'
-alias stormy='only_files | xargs storm'
 
 alias gmine='gmod | spelunk'
-alias gstorm='gmod | spelunk'
---
+++ shared/plugins/webstorm.sh
@@ -0,0 +1,3 @@
+alias storm='only_files | xargs webstorm'
+
+alias gstorm='gmod | storm'
