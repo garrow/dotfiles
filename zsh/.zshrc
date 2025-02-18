7079b96c7df2aae35afa846feb51445192afa284
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2025-02-18 16:51:04 -0500

    Fix zshrc again - add lock/unlock aliases

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,3 +1,4 @@
 # Main config file
 source "${base_dotfiles_path}/bootstrap.sh"
 source "${base_dotfiles_path}/zsh/zshrc-actual"
+# EOF
--- zsh/core/aliases.sh
+++ zsh/core/aliases.sh
@@ -1,1 +1,3 @@
 alias rld="source ${ZDOTDIR}/.zshrc"
+alias unlock_zshrc="chmod u+w ${ZDOTDIR}/.zshrc"
+alias lock_zshrc="chmod u-w ${ZDOTDIR}/.zshrc"
