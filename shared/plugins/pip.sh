ae74a059d6478166c122e3e73b80b375b02e41b8
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2025-02-18 16:52:43 -0500

    clean up sdkman shim

--- shared/plugins/pip.sh
+++
@@ -1,8 +0,0 @@
-export PIP_DOWNLOAD_CACHE=$HOME/.cache/pip
-
-
-# pipx
-if command -v pipx > /dev/null ; then
-  #echo "pipx detected"
-  pipx ensurepath
-fi
--- shared/plugins/shim_sdkman.sh
+++ shared/plugins/shim_sdkman.sh
@@ -1,12 +1,17 @@
 # Lazy Load SDKMAN
+
+__sdk_load(){
+    if [ -e "${HOME}/.sdkman/bin/sdkman-init.sh" ] ; then
+    load_dependency_file "${HOME}/.sdkman/bin/sdkman-init.sh" "SDKMAN - https://sdkman.io/"
+    fi
+}
+
 __sdk_first_run() {
     echo "🟢 Lazy loading sdk - SDKManager"
 
     unalias sdk # Ensure we use the binary now.
 
-    if [ -e "${HOME}/.sdkman/bin/sdkman-init.sh" ] ; then
-    load_dependency_file "${HOME}/.sdkman/bin/sdkman-init.sh" "SDKMAN - https://sdkman.io/"
-    fi
+    __sdk_load
 
     echo "🟢 Done"
 
@@ -14,6 +19,6 @@ __sdk_first_run() {
     sdk $@
 }
 
-debug_dotfiles "Inserted shim for sdk"
+
 
 alias sdk=__sdk_first_run
